63888bdf74c885117b8fd9d545248de1e3d263b2
PJ Mantoss <joelptoss@gmail>
2019-06-03 04:11:42 +0100

    add second migrate for tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_172908) do
+ActiveRecord::Schema.define(version: 2019_06_03_025003) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+  end
 
 end
