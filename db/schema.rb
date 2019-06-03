ba50983cd2cbd070fb3b5d5515a7946a0c37e396
PJ Mantoss <joelptoss@gmail>
2019-06-03 05:06:47 +0100

    article file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_025003) do
+ActiveRecord::Schema.define(version: 2019_06_03_030824) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
